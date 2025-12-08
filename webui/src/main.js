ac76b7da44c73d020bf8333b80a7e2006344dd5d
Alessandro Campolo <campoloalex@gmail.com>
2025-12-08 21:45:56 +0100

    Removed Dash test UI

--- webui/src/main.js
+++ webui/src/main.js
@@ -4,20 +4,33 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
-// Plugins
-import { registerPlugins } from '@/plugins'
 
-// Components
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
 import App from './App.vue'
-
-// Composables
-import { createApp } from 'vue'
-
-// Styles
+import {createApp} from 'vue'
 import 'unfonts.css'
+import routes from "@/routes.js";
+import {createRouter, createWebHistory} from "vue-router";
+
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'system',
+  },
+})
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
 
 const app = createApp(App)
 
-registerPlugins(app)
+app
+  .use(vuetify)
+  .use(router)
 
 app.mount('#app')
